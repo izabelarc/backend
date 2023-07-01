a4f62d2070d8fbc6cbf135410e37e286c022b499
izabela <izabelainfinity@gmail.com>
2023-07-01 13:14:47 -0300

    desafioinsert

--- python/listasetuplas/revisãolista.py
+++ python/listasetuplas/revisãolista.py
@@ -1,20 +1,43 @@
 from random import randint 
 
 series = ['Wednesday', 'The Walking Dead', 'Anne with an E', 'Blind Spot', 'Round 6']
+'''for serie in series:
+    print(serie) #percorrer a lista toda'''
 
 rates = []
 random_num_list = []
 count = 0
-'''for serie in series:
-    print(serie) #percorrer a lista toda'''
 
 while count < 5:
     random_num = randint(0,4)
     if random_num not in random_num_list:
         rate = int(input(f'Quantas estrelas para a serie {series[random_num]} '))
         count += 1
-        rates.insert(random_num, rate)
-    random_num_list.append(random_num)
+        rates.append((series[random_num], rate))
+        random_num_list.append(random_num)
+    
+for rate in rates:
+    series_name = rate[0]
+    series_rate = rate[1]
+    print(f"Serie {series_name} - {series_rate}")
+    
+'''from random import randint 
+
+series = ['Wednesday', 'The Walking Dead', 'Anne with an E', 'Blind Spot', 'Round 6']
+
+#ERRADO rates = [] ERRADO#
+rates = [0,0,0,0,0]
+random_num_list = []
+count = 0
+
+while count < 5:
+    random_num = randint(0,4)
+    if random_num not in random_num_list:
+        rate = int(input(f'Quantas estrelas para a serie {series[random_num]} '))
+        count += 1
+        rates[random_num] = rate
+        #ERRADO rates.insert(random_num, rate) ERRADO#
+        random_num_list.append(random_num)
     
 for index in range(5):
-    print(f"Serie {series[index]} - {rates[index]}")
+    print(f"Serie {series[index]} - {rates[index]}")'''
