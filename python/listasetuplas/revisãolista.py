504a23e61937e7b13357d8d2f5e95db4d1e38ff7
izabela <izabelainfinity@gmail.com>
2023-07-01 12:11:51 -0300

    revlista

--- python/listasetuplas/revisãolista.py
+++ python/listasetuplas/revisãolista.py
@@ -1,19 +1,20 @@
 from random import randint 
 
-series = ["Wednesday", "The Walking Dead", "Anne with an E", "Blind Spot", "Round 6"]
+series = ['Wednesday', 'The Walking Dead', 'Anne with an E', 'Blind Spot', 'Round 6']
 
 rates = []
 random_num_list = []
-
+count = 0
 '''for serie in series:
     print(serie) #percorrer a lista toda'''
 
-for count in range (0,6):
-    random_num = randint(0,5)
+while count < 5:
+    random_num = randint(0,4)
     if random_num not in random_num_list:
-        rate = int(input(f"Quantas estrelas para {series[random_num]}?"))
-    rates.append(rate)
+        rate = int(input(f'Quantas estrelas para a serie {series[random_num]} '))
+        count += 1
+        rates.insert(random_num, rate)
     random_num_list.append(random_num)
     
-print(series)
-print(rates)
+for index in range(5):
+    print(f"Serie {series[index]} - {rates[index]}")
