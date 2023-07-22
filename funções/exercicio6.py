999129d9e3d42d7b57ff84d93f0a698899fe1048
Izabela <izabelainfinity@gmail.com>
2023-07-22 13:07:33 -0300

    exercicio6

---
+++ funções/exercicio6.py
@@ -0,0 +1,3 @@
+'''Crie uma função que recebe uma lista via parametro, em seguida sorteia cinco numeros aleatorios 
+(entre 1 e 10) e preenche esta lista com os números sorteados. 
+Crie também outra função que recebe uma lista via parametro e retorne a soma de todos os números pares desta lista'''
