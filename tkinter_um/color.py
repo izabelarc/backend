dc4634df94b5a7b3fc6de995590512399f933ff7
ItaloMarquesGit <italomarquesgit@gmail.com>
2023-08-19 12:23:05 -0300

    revisao09

---
+++ tkinter_um/color.py
@@ -0,0 +1,11 @@
+#Color name
+#Black, white, yellow, cian, blue, grey, pink
+
+#Hexadecimal / Hex color
+# #7c7c7c #DCDCDC #FFFFFF
+
+# RGB Color
+# rgb(0,0,0) rgb(255,0,0) rgb(0,255,0)
+
+# RGBA Color
+# rgba(255,0,0,1)
---
+++ tkinter_um/materia2.py
@@ -0,0 +1,14 @@
+'''Métodos de posicionamento (psck, grid, place)
+
+- Posicionar e organizar os elementos gráficos(widgets) dentro de um ou mais containêrs(frame, janela)
+PACK: Posiciona usando conceito de empacotamento. Parâmetros:
+    side() - top, bottom, left, right
+    padx e pady
+GRID: 
+    row e column
+    rowspan e columspan
+    sticky
+PLACE:
+    x e y
+    width e height
+    relx e rely'''
