5a8834c2de61fe29743bb449288efa7fe030e4b2
carlosilveira98 <silveira.caam@gmail.com>
2023-08-26 12:23:33 -0300

    pooo

---
+++ poo_um.py/main.py
@@ -0,0 +1,16 @@
+from calculadora import Calculadora 
+
+nova_calculadora = Calculadora(1,2)
+outra_calculadora = Calculadora(4,5)
+
+print(nova_calculadora.sum())
+print(nova_calculadora.sub())
+print(nova_calculadora.mult())
+print(nova_calculadora.div())
+
+print("\noutra_calculadora")
+print(outra_calculadora.sum())
+print(outra_calculadora.sub())
+print(outra_calculadora.mult())
+print(outra_calculadora.div())
+
