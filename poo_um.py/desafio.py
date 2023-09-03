5aebab13bd3d9c493f400bfee701e91581bd4c10
izabelarc <izabelainfinity@gmail.com>
2023-09-03 20:05:14 -0300

    arrumado

--- poo_um.py/desafio.py
+++
@@ -1,4 +0,0 @@
-# A partir do exercicio04.py, implemente uma função à classe Animal chamada
-# comendo_aleatoriamente(). Essa função, deverá escolher uma comida aleatória da
-# lista de comidas desse animal e personalizar a mensagem que será exibida no terminal:
-# “O animal está comendo {comida}”.
